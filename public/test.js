40a6c320964a62707ec0ee165cc949d3a899bc9a
Vaughn Johnson <evilepicproductions@gmail.com>
2017-10-21 23:47:05 -0700

    added listeners to JS

--- public/test.js
+++ public/test.js
@@ -1,3 +1,17 @@
 var ws = new WebSocket("ws://" + location.host + "/ws");
+$("#tit")
+function submit(event){
+    return event.name;
+}
+// ws.send("wtf am I doing?")
 
-ws.send("wtf am I doing?")
+
+// Connection opened
+ws.addEventListener('open', function (event) {
+    ws.send('Hello Server! WTF am I doing?');
+});
+
+// Listen for messages
+ws.addEventListener('message', function (event) {
+    console.log('Message from server ', event.data);
+});
