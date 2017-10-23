7b79a14247b1b0e982b3daaccf0378d8e25ebc91
Flynn Liu <flynn16@users.noreply.github.com>
2017-10-23 14:30:45 -0700

    Clean up JS code. JQuery out.

--- public/test.js
+++ public/test.js
@@ -6,8 +6,7 @@ function wsReady(ws) {
     }
 
     function timer() {
-
-        var word = document.querySelector('#searchbox').value;
+        var word = document.querySelector('#search-box').value;
         if (word != currentWord) {
             currentWord = word;
             console.log(currentWord);
@@ -21,131 +20,129 @@ function wsReady(ws) {
 
 }
 
-$(document).ready(function() {
+function genCard(result) {
+    var li = document.createElement('li');
+
+    var card = document.createElement('div');
+    card.className += " card";
+
+    var card_body = document.createElement('div');
+    card_body.className += " card-body";
+
+    var thumb = document.createElement('img');
+    thumb.className += " card-thumb";
+    if(result['thumbnail'] != null) {
+        thumb.src = result['thumbnail'];
+    } else {
+        thumb.src = '/public/images/detour.png';
+    }
+    var card_title = document.createElement('h4');
+    card_title.className += " card-title";
+
+    var card_subtitle = document.createElement('h6');
+    card_subtitle.className += " card-subtitle mb-2 text-muted";
+    card_subtitle.textContent = result['source'];
+
+    var card_text = document.createElement('p');
+    card_text.className += " card-text";
+    card.textContent = result['desc'];
+
+    var card_link = document.createElement('a');
+    card_link.className += " card-link";
+    card_link.href = result['url'];
+    card_link.textContent = result['title'];
+    card_title.appendChild(card_link)
+
+    content = [card_title, card_subtitle, thumb, card_text];
+
+    for(i in content) {
+        card_body.append(content[i]);
+    }
+
+    card_body.appendChild(thumb);
+    card.appendChild(card_body);
+    card.className += " col-sm";
+    return card;
+}
+
+
+function main() {
+    function clearSuggests () {
+        var sugg = document.querySelector('#suggestion');
+        sugg.innerHTML = '';
+        return sugg;
+    }
+
     function openHandler(event) {
         wsReady(ws);
     }
 
-    function genCard(result){
-        var li = document.createElement('li');
-
-        var card = document.createElement('div');
-        card.className += " card";
-
-        var card_body = document.createElement('div');
-        card_body.className += " card-body";
-
-
-        var thumb = document.createElement('img');
-        thumb.className += " card-thumb";
-        if(result['thumbnail'] != null) {
-            thumb.src = result['thumbnail'];
-        } else {
-            thumb.src = '/public/images/detour.png';
-        }
-        var card_title = document.createElement('h4');
-        card_title.className += " card-title";
-
-        var card_subtitle = document.createElement('h6');
-        card_subtitle.className += " card-subtitle mb-2 text-muted";
-        card_subtitle.textContent = result['source'];
-
-        var card_text = document.createElement('p');
-        card_text.className += " card-text";
-        card.textContent = result['desc'];
-
-        var card_link = document.createElement('a');
-        card_link.className += " card-link";
-        card_link.href = result['url'];
-        card_link.textContent = result['title'];
-        card_title.appendChild(card_link)
-
-        content = [card_title, card_subtitle, thumb, card_text];
-
-        for(i in content) {
-            card_body.append(content[i]);
-        }
-
-        card_body.appendChild(thumb);
-        card.appendChild(card_body);
-        card.className += " col-sm";
-        return card;
-    }
-
     // Listen for messages
     function msgHandler(event) {
-        data = JSON.parse(event.data);
+        var data = JSON.parse(event.data);
         console.log(data);
 
-        //For suggestions
-        if (document.querySelector('#searchbox').value.startsWith(data['from'])) {
-            var sugg = document.querySelector('#suggestion');
-            sugg.innerHTML = '';
-
-            for (var result of data['results']) {
-                var div = document.createElement('div');
-                div.innerText = result;
-                div.className = "autocomplete"
-                sugg.appendChild(div);
+        // For suggestions
+        if (data.hasOwnProperty('from')) {
+            var text = document.querySelector('#search-box').value;
+            if (text.startsWith(data['from'])) {
+                var sugg = clearSuggests();
+                for (var result of data['results']) {
+                    var div = document.createElement('div');
+                    div.innerText = result;
+                    div.className = 'autocomplete';
+                    div.onclick = function (event) {
+                        submitQuery(div.textContent);
+                    }
+                    sugg.appendChild(div);
+                }
             }
         }
 
-        //For search results
-        if (data.hasOwnProperty('from_id')) {
-            resultsList = document.getElementById('results');
-            var idx = 0;
-            var cont = document.getElementById("container");
+        // For search results
+        else if (data.hasOwnProperty('from_id')) {
+            var resultsList = document.getElementById('results');
+            var index = 0;
+            var container = document.getElementById("container");
             var row = document.createElement('div');
 
             for (var result of data['results']) {
-                if(idx % 3 == 0){
-                    cont.appendChild(row);
+                if(index % 3 == 0) {
+                    container.appendChild(row);
                     row = document.createElement('div');
                     row.className = "row";
                 }
                 row.appendChild(genCard(result));
-                idx++;  
+                index++;
             }
+            clearSuggests();
         }
-
     }
-
-    $("autocomplete").click(function(event){
-        var query = strSearch($("#searchbox").val(), 0)
-        ws.send(query);
-    });
-
 
     // Connection opened
     var ws = makeWs(openHandler, msgHandler);
 
-
-    $("#searchbox").keyup(function (event){
-        var text = document.querySelector("#searchbox").value;
-        // var query = strSuggest($("#searchbox").val())
-        // ws.send(query);
-        currentWord = text;
-    });
-
-    //Submissions
-
-    $("#searchbox").keypress(function (event){
-        if(event.keyCode == 13){
-            var query = strSearch($("#searchbox").val(), 0)
-            ws.send(query);
+    // Suggestions and Submissions
+    function submitQuery(text = null) {
+        if (text == null) {
+            text = document.querySelector('#search-box').value;
         }
-    });
-
-    $("#submit").click(function (event){
-        var query = strSearch($("#searchbox").val(), 0)
+        var query = strSearch(text, 0);
         ws.send(query);
-    });
+        console.log('Query:', text);
+    }
 
-    $(document).click(".autocomplete", function (event){
-        ws.send(strSearch($(event.target).text(),0));
-    });
+    document.querySelector('#search-button').onclick = function (event) {
+        submitQuery();
+    };
 
-    // var ws = new WebSocket("ws://" + location.host + "/ws");
+    document.querySelector("#search-box").onkeypress = function (event) {
+        currentWord = document.querySelector("#search-box").value;
+        if (event.keyCode == 13) {
+            submitQuery();
+        }
+    };
 
+}
 
-});
+document.addEventListener("DOMContentLoaded", main);
